966b50bdb64fc9294cba4c32170b9b0407d4c803
Pedro Cruz <pcruz93@gmail.com>
2016-09-26 15:38:57 -0500

    Documentación de config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 /**
  * Created by pcruz93 on 26/09/16.
  */
+// Creación de variable
 var config = {};
 
-//Datos para conectarse al servidor de correos
+// Datos para conectarse al servidor de correos
 config.smtps = process.env.SMTPS;
 
 module.exports = config;
