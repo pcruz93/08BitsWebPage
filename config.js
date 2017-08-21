99dda7d6df3b942b155b0f663a7306c1e9d929f3
Pedro Cruz <pcruz93@gmail.com>
2017-08-20 20:34:30 -0500

    Agregando contenido de 08Bits Stack y correcciones a código javascript

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
  * Created by pcruz93 on 26/09/16.
  */
 // Creación de variable para guardar los datos de configuración
-var config = {};
+var config = {}
 
 // Datos para conectarse al servidor de correos
-config.smtps = process.env.SMTPS;
+config.smtps = process.env.SMTPS
 
-module.exports = config;
+module.exports = config
